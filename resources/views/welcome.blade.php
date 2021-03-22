d818c223889c598b0a9e853d78a1eceb0c7559f3
Ailan Ñañez <ailanruiz1994@gmail.com>
2021-03-22 12:30:56 -0400

    Pruebas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,11 +21,12 @@
         </style>
     </head>
     <body class="antialiased">
+        Jessika es la mejor novia del mundo...
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Homesss</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
